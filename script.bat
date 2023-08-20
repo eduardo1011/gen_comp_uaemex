6fe5a91a92f74982da53b868563cbb8c0a573367
eduardo1011 <35233945+eduardo1011@users.noreply.github.com>
2023-08-20 16:51:05 -0600

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,6 +1,19 @@
 @ECHO OFF
 ECHO _________________________________________________________
 
+ECHO Programa: clustalo.exe
+ECHO library: libgcc_s_sjlj-1.dll, libgomp-1.dll, libstdc++-6.dll, pthreadGC2-w64.dll
+
+curl "https://raw.githubusercontent.com/eduardo1011/gen_comp_uaemex/main/clustalo.exe" -o clustalo.exe
+
+curl "https://raw.githubusercontent.com/eduardo1011/gen_comp_uaemex/main/libgcc_s_sjlj-1.dll" -o libgcc_s_sjlj-1.dll
+
+curl "https://raw.githubusercontent.com/eduardo1011/gen_comp_uaemex/main/libgomp-1.dll" -o libgomp-1.dll
+
+curl "https://raw.githubusercontent.com/eduardo1011/gen_comp_uaemex/main/libstdc++-6.dll" -o libstdc++-6.dll
+
+curl "https://raw.githubusercontent.com/eduardo1011/gen_comp_uaemex/main/pthreadGC2-w64.dll" -o pthreadGC2-w64.dll
+
 ECHO Programa: ncbi-blast-2.8.1+-win64.exe
 ECHO _________________________________________________________
 
